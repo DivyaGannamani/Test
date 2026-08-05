c915d604b15c1cfe40a4e3af30adee6dea466b35
Divya G <divyagannamani2000@gmail.com>
2026-08-05 19:03:38 -0400

    Initial commit

---
+++ src/test/java/com/demoproject/Test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
